76070385a7af07ecfdca4c93dd90d57159eeb716
riyarane46 <154919910+riyarane46@users.noreply.github.com>
2025-04-11 21:54:21 +0530

    Add files via upload

---
+++ globals.py
@@ -0,0 +1,15 @@
+
+# rate of change of weights during training
+WEIGHT_RATE = 0.01
+
+# rate of change of biases during training
+BIAS_RATE = 0.05
+
+# total number of epochs for training
+NUM_EPOCHS = 500
+
+# printing frequency of epochs during training
+EPOCH_FREQ = 50
+
+# probability of setting an input to zero during the dropout function
+DROPOUT_PROBABILITY = 0.05
